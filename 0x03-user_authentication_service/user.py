484da886b4377faa9963acc8147d551dd3f50894
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2023-02-15 16:29:36 +0300

    Fix issues

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python3
-"""
-    SQLAlchemy ORM model 'User' for a database table named 'user'
+"""The `user` model's module.
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-# A declarative base class to map a class to DB tables
+
 Base = declarative_base()
 
 
 class User(Base):
+    """Represents a record from the `user` table.
     """
-        A User class that represents a db table 'user'
-        with the following columns defined
-    """
-    # describe the db table name
-    __tablename__ = 'users'
-
-    # describe the columns
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
